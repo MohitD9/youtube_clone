0d5956a20e609934581ad86bcca0eaa9b9cca1bb
MohitD9 <deshpandemohit1999@gmail.com>
2023-07-10 19:25:00 +0530

    Starting creating Feed section.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import { useSate, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import SideBar from "./SideBar";
 
 const Feed = () => {
-    return (
-        <div>
-            Feed
-        </div>
-    )
-}
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright © 2023 MD Media
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
 export default Feed;
