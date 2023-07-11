1478c295a41bd09192ee883a14d1e07c0eb315a0
MohitD9 <deshpandemohit1999@gmail.com>
2023-07-11 22:25:23 +0530

    Making some changes in feed section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,23 @@
-import { useSate, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import SideBar from "./SideBar";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+import { SideBar, Videos } from "./";
+
+// import SideBar from "./SideBar";
+// import Videos from "./Videos";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -12,7 +27,11 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <SideBar />
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+
         <Typography
           className="copyright"
           variant="body2"
@@ -20,6 +39,19 @@ const Feed = () => {
         >
           Copyright © 2023 MD Media
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
